11ddbe287750c2d729700e6602b4a198ec633593
April Zhao <abooli@outlook.com>
2022-04-05 19:55:54 -0700

    mm

--- load_denormalized.sh
+++ load_denormalized.sh
@@ -1,1 +1,3 @@
+# loads denormalized data in a parallel manner
+
 unzip -p "$1" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1176/ -c "COPY tweets_jsonb(data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
