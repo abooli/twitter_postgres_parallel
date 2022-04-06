7f430e084677b034177b3196e5cc3ecc635bae1b
April Zhao <abooli@outlook.com>
2022-04-05 22:58:10 -0700

    test again

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -8,12 +8,12 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this
-#echo "$files" | time parallel sh load_denormalized.sh # SCRIPT TO RUN IN PARALLEL
+echo "$files" | time parallel sh load_denormalized.sh # SCRIPT TO RUN IN PARALLEL
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-#echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:1177/ --inputs
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:1177/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
